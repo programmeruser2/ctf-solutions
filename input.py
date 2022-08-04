ee9aa0bb76ca1a9098e602a38cd3ca6cd99d2a00
programmeruser2 <programmeruser@programmeruser.tk>
2022-08-04 17:20:19 -0400

    update

--- input.py
+++ input.py
@@ -12,11 +12,7 @@ print(s.system('echo -e "\\x00\\x0a\\x00\\xff" > /tmp/input2files/stdin').recval
 print(s.system('echo -e "\\x00\\x0a\\x02\\xff" > /tmp/input2files/stderr').recvall())
 #print(s.system('echo -e "\\x00\\x00\\x00\\x00 > /tmp/input2files/tmp').recvall())
 s.write('/tmp/input2files/\x0a', '\x00\x00\x00\x00')
-p = s.process(argv=args, 
-							stdin='/tmp/input2files/stdin', 
-							stderr='/tmp/input2files/stderr',
-							env={'\xde\xad\xbe\xef': '\xca\xfe\xba\xbe'},
-							cwd='/tmp/input2files')
+p = s.process(argv=args, stdin='/tmp/input2files/stdin', stderr='/tmp/input2files/stderr', env={'\xde\xad\xbe\xef': '\xca\xfe\xba\xbe'}, cwd='/tmp/input2files')
 #sleep(5) # just in case
 s['echo -e "\\xDE\\xAD\\xBE\\xEF" | nc localhost 4369'] # 0x1111 = 4369
 p.interactive()
